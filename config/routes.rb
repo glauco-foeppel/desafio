783a0c57ce936d0dec8da0dee9ff9ffb7d177c25
Glauco Foeppel <glauco.tf@gmail.com>
2016-11-30 17:18:22 -0200

    Adiciona rotar para area de admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   resources :users, only: [:index, :edit, :update]
 
+  namespace :admin do
+    resources :users, only: [:index, :show, :edit, :updste, :destroy]
+  end
+
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
