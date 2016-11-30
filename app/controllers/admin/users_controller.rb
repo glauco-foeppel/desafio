acf3a69798a5e68535ad275fc4241636fe4925e5
Glauco Foeppel <glauco.tf@gmail.com>
2016-11-30 17:24:12 -0200

    Adiciona diretorio contendo o controller users para admin

---
+++ app/controllers/admin/users_controller.rb
@@ -0,0 +1,29 @@
+class Admin::UsersController < ApplicationController
+  #before_action :authenticate_user!
+
+  def index
+    @user = User.find_by_id(current_user.id)
+  end
+
+  def edit
+    @user = User.find_by_id(params[:id])
+  end
+
+  def update
+    respond_to do |format|
+      @user = User.find_by_id(params[:id])
+      if @user.update(user_params)
+        format.html { redirect_to users_path, notice: 'Seu perfil foi atualizado com sucesso.' }
+        format.json { render :show, status: :ok, location: @user }
+      else
+        format.html { render :edit }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+  def user_params
+      params.require(:user).permit(:name, :email, :password,:password_confirmation)
+  end
+end
