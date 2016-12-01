b150c3e42416f064a5709730a4c40d0c4fb41b68
Glauco Foeppel <glauco.tf@gmail.com>
2016-12-01 08:03:46 -0200

    Correção do redirect apos logout da area admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,8 +6,20 @@ class ApplicationController < ActionController::Base
   before_filter :configure_permitted_parameters, if: :devise_controller?
 
   protected
+      # Utilizado para adicionar campos ao cadastro de usuarios via devise
       def configure_permitted_parameters
           devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :email, :password) }
           devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:name, :email, :password, :current_password) }
       end
+
+  private
+
+  def after_sign_out_path_for(resource_or_scope)
+    if request.path == "/admins/sign_out"
+      admin_users_path
+    else
+      root_path
+    end
+  end
+
 end
