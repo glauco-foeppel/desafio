5a4f734c09c762bd5af842084d4cfe4c1df671aa
Glauco Foeppel <glauco.tf@gmail.com>
2016-12-01 07:37:04 -0200

    Adiciona seed para papular a base de dados para teste

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+# create teh users
+User.create(
+  [
+    { name: "José", email: "jose@email.com", password: "12345678" },
+    { name: "Paulo", email: "paulo@email.com", password: "12345678" },
+    { name: "Pedro", email: "pedro@email.com", password: "12345678" },
+    { name: "Vera", email: "vera@email.com", password: "12345678" },
+    { name: "João", email: "joao@email.com", password: "12345678" },
+    { name: "Josy", email: "josy@email.com", password: "12345678" },
+    { name: "Roberta", email: "beta@email.com", password: "12345678" },
+    { name: "Carla", email: "carla@email.com", password: "12345678" },
+    { name: "Bruno", email: "bruno@email.com", password: "12345678" },
+    { name: "Carlos", email: "carlos@email.com", password: "12345678" },
+  ]
+)
+
+Admin.create(
+  name: "Admin",
+  email: "admin@email.com",
+  password: "12345678"
+)
